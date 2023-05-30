b82b4fd7f7ad1bb65c44829f4bf7dacee68e6b76
Vishal <vishaljain7100@gmail.com>
2023-05-30 12:49:11 +0530

    Second commit pe hu

---
+++ Script.js
@@ -0,0 +1,6 @@
+// let HTMLWrite = document.getElementById('HTMLWrite')
+// HTMLWrite.addEventListener('click', ()=>{
+//     if(HTMLWrite.innerHTML = "//Wrtie Your HTML Here"){
+//         HTMLWrite.innerHTML = ""
+//     }
+// })

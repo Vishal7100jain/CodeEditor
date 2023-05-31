854f305d48cc3e1a08ac452d7d3da7689d042701
Vishal <vishaljain7100@gmail.com>
2023-06-01 00:02:53 +0530

    js is just started with changes in color for code

--- Script.js
+++ Script.js
@@ -1,6 +1,43 @@
-// let HTMLWrite = document.getElementById('HTMLWrite')
-// HTMLWrite.addEventListener('click', ()=>{
-//     if(HTMLWrite.innerHTML = "//Wrtie Your HTML Here"){
-//         HTMLWrite.innerHTML = ""
-//     }
-// })
+let HTML_code = document.getElementById('HTML-code')
+let CSS_code = document.getElementById('CSS-code')
+let JS_code = document.getElementById('JS-code')
+let Output = document.getElementById('Output')
+let HtmlLang = document.getElementById('HtmlLang')
+let CssLang = document.getElementById('CssLang')
+let JsLang = document.getElementById('JsLang')
+let textarea = document.querySelectorAll('textarea')
+
+
+// function OutputPrint(){
+// }
+
+
+
+Array.from(textarea).forEach((textarea)=>{
+    textarea.addEventListener('click', (e)=>{
+        
+        setInterval(()=>{
+            Output.value = textarea.value
+        },2000)
+        
+        //Color changing logic for textarea
+        if(e.target.name == "HTML" ){
+            HtmlLang.style.backgroundColor = "#34363e"
+            CssLang.style.backgroundColor = "#2f2b2b"
+            JsLang.style.backgroundColor = "#2f2b2b"
+        }
+        
+        else if(e.target.name == "CSS" ){
+            HtmlLang.style.backgroundColor = "#2f2b2b"
+            CssLang.style.backgroundColor = "#34363e"
+            JsLang.style.backgroundColor = "#2f2b2b"
+        }
+        
+        else if(e.target.name == "JS" ){
+            HtmlLang.style.backgroundColor = "#2f2b2b"
+            CssLang.style.backgroundColor = "#2f2b2b"
+            JsLang.style.backgroundColor = "#34363e"
+        }
+    })
+})
+

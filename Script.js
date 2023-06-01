96c62164b72eae897a6404ca8ca36f6445f522f6
Vishal <vishaljain7100@gmail.com>
2023-06-01 21:01:17 +0530

    add logic to run the users input code

--- Script.js
+++ Script.js
@@ -1,15 +1,48 @@
 let HTML_code = document.getElementById('HTML-code')
 let CSS_code = document.getElementById('CSS-code')
 let JS_code = document.getElementById('JS-code')
-let Output = document.getElementById('Output')
 let HtmlLang = document.getElementById('HtmlLang')
 let CssLang = document.getElementById('CssLang')
 let JsLang = document.getElementById('JsLang')
 let textarea = document.querySelectorAll('textarea')
 
 
-// function OutputPrint(){
-// }
+let Output = document.querySelector('iframe').contentDocument
+let style = Output.createElement('style')
+Output.head.appendChild(style)
+
+let script = Output.createElement('script')
+Output.body.appendChild(script)
+
+
+
+
+// Main logic to run html, css and js entered by user
+function run(){
+    Output.body.innerHTML = HTML_code.value
+    style.innerHTML = CSS_code.value
+    script.innerHTML = JS_code.value
+
+    if(HTML_code.value === "!DOC"){
+        HTML_code.addEventListener('keypress', (event)=>{
+            if(event.key === "Enter"){
+                HTML_code.value = `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    
+                </body>
+                </html>`
+            }
+        })
+    }
+}
+
 
 
 
@@ -17,8 +50,8 @@ Array.from(textarea).forEach((textarea)=>{
     textarea.addEventListener('click', (e)=>{
         
         setInterval(()=>{
-            Output.value = textarea.value
-        },2000)
+            run()
+        },10)
         
         //Color changing logic for textarea
         if(e.target.name == "HTML" ){

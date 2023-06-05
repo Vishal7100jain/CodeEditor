f725ac514def07fd8ba6418cec1d07cf843505e0
Vishal <vishaljain7100@gmail.com>
2023-06-05 20:44:55 +0530

    working on new page, bg changing on click nad auto html

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Main logic to run html, css and js entered by user
+
     var editor1 = CodeMirror.fromTextArea(document.getElementById('HTML-code'), {
         mode: "xml",
         theme: "midnight",
@@ -34,38 +35,14 @@ document.addEventListener('DOMContentLoaded', function () {
         iframe.head.innerHTML = cssvalue
         iframe.head.innerHTML = jsvalue
 
-        if (editor1.getValue() == "!DOC") {
-            let a = editor1.getValue()
-            a = `<!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Document</title>
-                    </head>
-                    <body>
-
-                </body>
-                </html>`
-        }
     }, 10)
 
+        let array = [editor1, editor2, editor3]
+       for(let value of array){
+           if(value == editor1 ){
+               console.log(value)
+           }
+       }
+
+
 })
-// Array.from(textarea).forEach((textarea) => {
-//     textarea.addEventListener('click', (e) => {
-//         // if(e.target == HTML_code){
-//         //     console.log(e.target,HTML_code)
-//         //     // HtmlLang.style.backgroundColor = "red"
-//         //     // CssLang
-//         //     // JsLang
-//         // }
-//     })
-// })
-
-// });
-
-
-// htmlvalue.addEventListener('click', ()=>{
-//     console.log("clicked")
-// })

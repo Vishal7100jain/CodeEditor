6b286bb0650843372c97f6508b3ee158fdd61990
Vishal <vishaljain7100@gmail.com>
2023-06-17 13:42:51 +0530

    Changes in Dark/light theme, Working Properly

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Main logic to run html, css and js entered by user
     var editor1 = CodeMirror.fromTextArea(document.getElementById('HTML-code'), {
         mode: "xml",
-        theme: "midnight",
+        theme: "mdn-like",
         autoCloseBrackets: true,
         autoCloseTags: true,
         lineNumbers: true
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var editor2 = CodeMirror.fromTextArea(document.getElementById('CSS-code'), {
         mode: "css",
-        theme: "midnight",
+        theme: "mdn-like",
         autoCloseBrackets: true,
         autoCloseTags: true,
         lineNumbers: true
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var editor3 = CodeMirror.fromTextArea(document.getElementById('JS-code'), {
         mode: "javascript",
-        theme: "midnight",
+        theme: "mdn-like",
         autoCloseBrackets: true,
         autoCloseTags: true,
         lineNumbers: true
@@ -63,5 +63,22 @@ document.addEventListener('DOMContentLoaded', function () {
         CodeMirror.classList.toggle('height')
         CodeMirror1.classList.toggle('height')
         CodeMirror2.classList.toggle('height')
+    })
+
+    // Logic to change the Theme...
+    let DarkLight = document.getElementById('DarkLight')
+    DarkLight.addEventListener('click', () => {
+        if(editor1.getOption("theme") == "mdn-like"){
+            editor1.setOption('theme', "midnight");
+            editor2.setOption('theme', "midnight");
+            editor3.setOption('theme', "midnight");
+        }
+        
+        else if(editor1.getOption("theme") == "midnight"){
+            editor1.setOption('theme', "mdn-like");
+            editor2.setOption('theme', "mdn-like");
+            editor3.setOption('theme', "mdn-like");
+        }
+        
     })
 })
